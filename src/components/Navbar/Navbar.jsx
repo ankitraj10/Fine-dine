8c878ae60a8d5acf13d8bcad105415c14b53a723
ankitraj10 <ankitraj.mail1@gmail.com>
2023-02-26 14:54:46 +0530

    Navbar added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,90 @@
-import React from 'react';
+import React, { useState } from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { MdOutlineRestaurantMenu } from "react-icons/md";
+import images from "../../constants/images";
+import "./Navbar.css";
 
-import './Navbar.css';
+const Navbar = () => {
+  const [togglemenu, setToggleMenu] = useState(false);
 
-const Navbar = () => (
-  <div>
-    Navbar
-  </div>
-);
+  return (
+    <nav className="app__navbar">
+      <div className="app__navbar-logo">
+        <img src={images.gericht} alt="app log" />
+      </div>
+      <ul className="app__navbar-links">
+        <li className="p__opensans">
+          <a href="#home">Home</a>
+        </li>
+        <li className="p__opensans">
+          {" "}
+          <a href="#About">About</a>
+        </li>
+        <li className="p__opensans">
+          {" "}
+          <a href="#Menu">Menu</a>
+        </li>
+        <li className="p__opensans">
+          {" "}
+          <a href="#Awards">Awards</a>
+        </li>
+        <li className="p__opensans">
+          {" "}
+          <a href="#Contact">Contact</a>
+        </li>
+      </ul>
+      <div className="app__navbar-login">
+        <a href="#login" className="p__opensans">
+          Log In/Reister
+        </a>
+        <div />
+        <a href="/" className="p__opensans">
+          Book Table
+        </a>
+      </div>
+      <div className="app__navbar-smallscreen">
+        <GiHamburgerMenu
+          color="#fff"
+          fontSize={27}
+          onClick={() => {
+            setToggleMenu(true);
+          }}
+        />
+        {togglemenu && (
+          <div className="app__navbar-smallscreen_overlay flex__center slide-bottom">
+            <MdOutlineRestaurantMenu
+              fontSize={27}
+              className="overlay__close"
+              onClick={() => {
+                setToggleMenu(false);
+              }}
+            />
+            <ul className="app__navbar-smallscreen_links">
+              <li className="p__opensans">
+                <a href="#home">Home</a>
+              </li>
+              <li className="p__opensans">
+                {" "}
+                <a href="#About">About</a>
+              </li>
+              <li className="p__opensans">
+                {" "}
+                <a href="#Menu">Menu</a>
+              </li>
+              <li className="p__opensans">
+                {" "}
+                <a href="#Awards">Awards</a>
+              </li>
+              <li className="p__opensans">
+                {" "}
+                <a href="#Contact">Contact</a>
+              </li>
+            </ul>
+          </div>
+        )}
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
